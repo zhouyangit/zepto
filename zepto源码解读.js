9a5a59780ae9d856aed3afd7e05cb43990e55eef
zhouyang <marven@163.com>
2017-08-31 11:03:51 +0800

    更新

--- zepto源码解读.js
+++ zepto源码解读.js
@@ -95,10 +95,87 @@ var Zepto = (function() {
           // console.log(~-1); //0
           // console.log(~0) // -1
           // console.log(~1) // -2
-          // 这样如果每一找到该元素，都回返回 -1 经过 按位取反运算符 之后为0	
+          // 这样如果没有找到该元素，都回返回 -1 经过 按位取反运算符 之后为0	
+          // 当match = 0 即false 表示没有匹配
+          // 当match 等于其它值，即 true ,表示成功匹配
+          match = ~zepto.qsa(parent,selector).indexOf(element)
+          // 如果没有父节点， 就执行 tempParent 移除当前元素，因为前面把当前元素加入到这个tempParent中
+          temp && tempParent.removeChild(element)
+
+          // 返回 ~zepto.qsa的结果
+          return match
         }
 
-	
+		// 在代码中部，执行了
+	    // $.each("Boolean Number String Function Array Date RegExp Object Error".split(" "), function(i, name) {
+	    //   class2type[ "[object " + name + "]" ] = name.toLowerCase()
+	    // })
+	    // 用来给 class2type 对象赋值
+	    //
+	    // type 用来判断类型
+	    function type(obj) {
+	    	return obj == null ? String(obj):[toString.call(obj)] || "object"
+	    }
+
+	    // 判断是否是window对象 指当前的浏览器窗口，window对象的window属性指向自身
+	    // 即 window.window === window
+	    function isWindow(obj) {return obj != null && obj == boj.window}
+	    // 判断是否是 document 对象
+	    // window.document.nodeType == 9 数字表示为9，常量表示为 DOCUMENT_NODE 
+	    function isDocument(obj) { return obj != null && obj.nodeType == obj.DOCUMENT_NODE }
+	    
+	    // 判断是否是object
+	    //
+	    function isObject(obj){return type obj == 'object'}
+
+	    // 判断是否是最基本的object：Object.getPrototypeOf(obj) == Object.prototype
+        // Object.getPrototypeOf() 方法返回指定对象的原型(即，内部[[Prototype]]属性的值)
+        // getPrototypeOf 和 prototype 的区别：
+        // getPrototypeOf是个function ,而prototype 是个属性
+        function isPlainObject(obj) {
+        	return isObject(obj) && isWindow && Object.getPrototypeOf(obj) == Object.prototype
+        }
+
+        // 判断是否是数组或者对象数组
+        function likeArray(obj) {
+        	var length = !!obj && 'length' in obj && obj.length,
+        	    type = $.type(obj)
+
+        	return 'function' != type && !isWindow(obj) && (
+        	    'array' == type || length === 0 ||
+                (typeof length == 'number' && length >0 && (length - 1) in obj)
+        	)   
+        }
+
+        // 筛选数组，提出Null undefined 元素
+        function compact(array) {return filter.call(array,function(){ return item != null})}
+
+        function flatten(array) {return array.length > 0 ? $.fn.concat.apply([],array):array}
+
+        // 用于 css 的camalCase转换.例如 background-color 转换为 backgroundColor
+        camelize = function(str) { return str.replace(/-+(.)?/g,function(match,chr){return chr ? chr.toUpperCase() : '' })}
+
+        // 将 backgroundColor 转换为 background-color 格式
+        function dasherize(str) {
+        	return str.replace(/::/g,'/')
+        	.replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
+            .replace(/([a-z\d])([A-Z])/g, '$1_$2')
+            .replace(/_/g, '-')
+            .toLowerCase()
+        }
+
+        // 数组去重
+        uniq = function (array) {return filter.call(array,function(item, idx){ return array.indexOf(item) == idx})}
+
+        function classRE(name) {
+        	return name in classCache ?
+        	  classCache[name]:(classCache[name] = new RegExp('(^|\\s)'+name+'(\\s|$)'))
+        }
+
+        // 传入一个css 的name 和value。判断这个value是否需要增加 'px'
+        function maybeAddPx(name,value) {
+        	return (typeof value == 'number' && !cssNumber[dasherize(name)])?value+'px':value
+        }
 })()
 
 window.Zepto = Zepto
